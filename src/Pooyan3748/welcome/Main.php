b3d8fe235fb8b28e8d61a79a8694156c0694188b
Pooyan-MR-3748 <pooyanmr.3748@gmail.com>
2025-01-05 12:12:53 +0330

    Add files via upload

---
+++ src/Pooyan3748/welcome/Main.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pooyan3748\welcome;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\player\Player;
+use pocketmine\player\GameMode;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use onebone\economyapi\EconomyAPI;
+
+class Main extends PluginBase implements Listener{
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN ."on plugin");
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+
+        $player->sendMessage(TextFormat::GREEN ."Hi " .TextFormat::RED. $name .TextFormat::GREEN." Welcome to server" );
+        $player->setGamemode(GameMode::SURVIVAL);
+        $player->sendTitle(TextFormat::GREEN ."§lWelcome \n to server");
+        EconomyAPI::getInstance()->setMoney($player, 1);
+        $player->sendMessage($player.$name);
+        
+
+
+    }
+
+
+
+    public function onDisable() : void {
+        $this->getLogger()->info(TextFormat::RED ."off Plugin");
+    }
+}
